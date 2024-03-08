695b0b4087b25dfeafdce7175312d6da5b3cb8d9
Ruben Henares Hidalgo <rubenhenareshidalgo97@gmail.com>
2024-03-08 13:49:47 +0100

    tercer

--- incidenciesrubenhenares/main.js
+++ incidenciesrubenhenares/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -19,5 +19,3 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
